92334c9898d2bcc8192409781ebd1465212b9292
Kyler Smith <kysmith327@gmail.com>
2020-11-03 13:42:34 -0800

    Workshop 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -55,6 +55,7 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+    const recognizeComment = ({dx}) => (dx < 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -82,6 +83,9 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            }
+            else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
